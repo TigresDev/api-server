ff176804063bf24e00397b27d8ba68c54c438253
elliot <tigres.dev@gmail.com>
2019-03-29 15:50:19 +0900

    test2

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,8 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTets(TestCase):
-    def dummy:
-        
+
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
         email = 'rtigerhan@gmail.com'
